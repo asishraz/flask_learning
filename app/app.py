75ef713adfb5e0fea5535268a24167cef1e8e116
asish <iamasish3001@gmail.com>
2020-01-09 17:31:55 +0530

    learned how to use multiple urls in a single flask file

--- app/app.py
+++ app/app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/profile/<name>')
-def profile(name):
-	return render_template('profile.html', name=name)
 
+# #understanding the use of url_for() in flask and rendering the html file in flask_python file 
+# @app.route('/profile/<name>')
+# def profile(name):
+# 	return render_template('profile.html', name=name)
+
+
+#how to use multiple urls in a single flask file
+@app.route('/')
+@app.route("/<user>")
+def index(user=None):
+	return render_template("user.html", user=user)
 
 
 if __name__ == "__main__":
